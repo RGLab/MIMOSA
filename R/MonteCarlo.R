bb88e6b63122199fb2e5caf2bbb983243d469667
Greg Finak <gfinak@fhcrc.org>
2012-04-30 13:12:12 -0700

    Adaptive step size in test mcmc code. Fixed step size after reaching a good acceptance rate.
    Signed-off-by: Greg Finak <gfinak@fhcrc.org>

--- R/MonteCarlo.R
+++ R/MonteCarlo.R
@@ -1,5 +1,5 @@
 # TODO: Having issues with newton-raphson and optim for the dirichlet-multinomial model
-# Implement a Gibbs sampler.
+# Implement a MCMC sampler.
 # Author: finak
 ###############################################################################
 
@@ -7,8 +7,9 @@
 sProposal<-function(s,sigma=10){
 	return(rnorm(1,mean=s,sd=sigma))
 }
-alphaProposal<-function(alpha){
-	sapply(alpha,function(x)rnorm(1,mean=x,sd=10))
+alphaProposal<-function(alpha,sigma=diag(rep(10,length(alpha))),C=2.38/sqrt(length(alpha))){
+	#sapply(alpha,function(x)rnorm(1,mean=x,sd=sigma))
+	mvrnorm(mu=alpha,Sigma=sigma*C^2)
 }
 
 lddirichlet<-function(x,alpha){
@@ -16,47 +17,49 @@ lddirichlet<-function(x,alpha){
 	s<-sum(log(x)*(alpha-1))
 	s-logD
 }
-simAlpha.s<-function(alpha.s,alpha.u,z){
-	a<-llnull(c(alpha.s,alpha.u))
-	b<-llresp(c(alpha.s,alpha.u))
+simAlpha.s<-function(alpha.s,alpha.u,z,S,C,llnull,llresp,count){
+	a<-llnull(c(alpha.s,alpha.u))+sum(sapply(alpha.s,function(x)dgamma(x,1e-5)))
+	b<-llresp(c(alpha.s,alpha.u))+sum(sapply(alpha.s,function(x)dgamma(x,1e-5)))
 	
 	old<-sum(a*z[,1]+b*z[,2]+sum(dexp(alpha.s,rate=1e-3,log=TRUE)))
-	alpha.s.prop<-alphaProposal(alpha.s)
+	alpha.s.prop<-alphaProposal(alpha.s,S,C)
 	if(any(alpha.s.prop<0)){
-		return(alpha.s)
+		return(list(alpha.s,count))
 	}
 	
-	a<-llnull(c(alpha.s.prop,alpha.u))
-	b<-llresp(c(alpha.s.prop,alpha.u))
+	a<-llnull(c(alpha.s.prop,alpha.u))+sum(sapply(alpha.s.prop,function(x)dgamma(x,1e-5)))
+	b<-llresp(c(alpha.s.prop,alpha.u))+sum(sapply(alpha.s.prop,function(x)dgamma(x,1e-5)))
 	
 	new<-sum(a*z[,1]+b*z[,2]+sum(dexp(alpha.s.prop,rate=1e-3,log=TRUE)))
 	
 	if(log(runif(1))<=(new-old)&is.finite(new-old)){
-		return(alpha.s.prop)
+		count<-count+1
+		return(list(alpha.s.prop,count))
 	}else{
-		return(alpha.s)
+		return(list(alpha.s,count))
 	}
 	
 }
-simAlpha.u<-function(alpha.s,alpha.u,z){
-	a<-llnull(c(alpha.s,alpha.u))
-	b<-llresp(c(alpha.s,alpha.u))
+simAlpha.u<-function(alpha.s,alpha.u,z,S,C,llnull,llresp,count){
+	a<-llnull(c(alpha.s,alpha.u))+sum(sapply(alpha.u,function(x)dgamma(x,1e-5)))
+	b<-llresp(c(alpha.s,alpha.u))+sum(sapply(alpha.u,function(x)dgamma(x,1e-5)))
 	
 	old<-sum(a*z[,1]+b*z[,2]+sum(dexp(alpha.u,rate=1e-3,log=TRUE)))
-	alpha.u.prop<-alphaProposal(alpha.u)
+	alpha.u.prop<-alphaProposal(alpha.u,S,C)
 	if(any(alpha.u.prop<0)){
-		return(alpha.u)
+		return(list(alpha.u,count))
 	}
 	
-	a<-llnull(c(alpha.s,alpha.u.prop))
-	b<-llresp(c(alpha.s,alpha.u.prop))
+	a<-llnull(c(alpha.s,alpha.u.prop))+sum(sapply(alpha.u.prop,function(x)dgamma(x,1e-5)))
+	b<-llresp(c(alpha.s,alpha.u.prop))+sum(sapply(alpha.u.prop,function(x)dgamma(x,1e-5)))
 	
 	new<-sum(a*z[,1]+b*z[,2]+sum(dexp(alpha.u.prop,rate=1e-3,log=TRUE)))
 	
 	if(log(runif(1))<=(new-old)&is.finite(new-old)){
-		return(alpha.u.prop)
+		count<-count+1
+		return(list(alpha.u.prop,count))
 	}else{
-		return(alpha.u)
+		return(list(alpha.u,count))
 	}
 	
 }
@@ -172,30 +175,42 @@ fitMCMC<-function(data=NULL,inits=NULL,iter=5000,burn=2000,w.stepS=0.01,w.stepU=
 	}
 	llnull<-compiler::cmpfun(MIMOSA:::makeLogLikeNULLComponent(data$n.stim,data$n.unstim))
 	llresp<-compiler::cmpfun(MIMOSA:::makeLogLikeRespComponent(data$n.stim,data$n.unstim))
-	
+	tune=200
 	w.s=inits$ws
 	w.u=inits$wu
 	S.stim=inits$Sstim
 	S.unstim=inits$Sunstim
+	alpha.s=w.s*S.stim
+	alpha.u=w.s*S.unstim
 	q=inits$q
 	z=inits$z
 	f<-matrix(0,nrow=iter,ncol=length(w.s)+length(w.u)+3)
 	#f<-file("mcmc.dat",open="wt",blocking=FALSE)
-	cn<-c(paste("ws",1:length(w.s),sep=""),paste("wu",1:length(w.u),sep=""),"S.stim","S.unstim","q")
+	#cn<-c(paste("ws",1:length(w.s),sep=""),paste("wu",1:length(w.u),sep=""),"S.stim","S.unstim","q")
+	cn<-c(paste("alpha.s",1:length(w.s),sep=""),paste("alpha.u",1:length(w.u),sep=""),"q")
+	S.s<-S.u<-diag(10,length(alpha.s))
+	C<-2.34/sqrt(length(alpha.s))
 	#writeLines(text=as.character(c(w.s,w.u,S.unstim,S.stim,q,"\n")),sep="\t",f)
 	for(i in 1:iter){
-		w.s<-MIMOSA:::simWs(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,w.stepS,llnull,llresp)
-		w.u<-MIMOSA:::simWu(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,w.stepU,llnull,llresp)
-		S.unstim<-MIMOSA:::simS.unstim(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,sigmaS,llnull,llresp)
-		S.stim<-MIMOSA:::simS.stim(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,sigmaU,llnull,llresp)
-		z<-MIMOSA:::simZ(q=q,w.s=w.s,w.u=w.u,S.stim=S.stim,S.unstim=S.unstim,llnull,llresp)
+		#w.s<-MIMOSA:::simWs(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,w.stepS,llnull,llresp)
+		#w.u<-MIMOSA:::simWu(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,w.stepU,llnull,llresp)
+		if(iter%%tune==0){
+			#compute new S
+			S.s<-cov(f[(i-tune-1):i,1:(length(alpha.s))])
+			S.u<-cov(f[(i-tune-1):i,1:(length(alpha.u))])
+		}
+		alpha.s<-MIMOSA:::simAlpha.s(alpha.s,alpha.u,z,S.s,C=1,llnull,llresp)
+		alpha.u<-MIMOSA:::simAlpha.u(alpha.s,alpha.u,z,S.u,C=1,llnull,llresp)
+		#S.unstim<-MIMOSA:::simS.unstim(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,sigmaS,llnull,llresp)
+		#S.stim<-MIMOSA:::simS.stim(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,sigmaU,llnull,llresp)
+		z<-MIMOSA:::simZ(q=q,w.s=alpha.s/sum(alpha.s),w.u=alpha.u/sum(alpha.u),S.stim=sum(alpha.s),S.unstim=sum(alpha.u),llnull,llresp)
 		if(i==burn){
 			sz=z
 		}else if(i>burn){
 			sz=(sz+z/(i-1))*((i-1)/i)	
 		}
 		q<-MIMOSA:::simQ(z=z)
-		f[i,]<-c(w.s,w.u,S.unstim,S.stim,q)
+		f[i,]<-c(alpha.s,alpha.u,q)
 	}
 	colnames(f)<-cn
 	write(f[burn:iter,],file="mcmc.dat")
@@ -208,37 +223,67 @@ test<-function(iter,burn,w.stepS=0.01,w.stepU=0.001,sigmaS=20,sigmaU=20){
 	dat<-MIMOSA:::simMD(alpha.s=c(1000-120,40,40,40,40),alpha.u=c(1000,10,10,10,10),w=0.25)
 	
 	
-	llnull<-MIMOSA:::makeLogLikeNULLComponent(dat$n.stim,dat$n.unstim)
-	llresp<-MIMOSA:::makeLogLikeRespComponent(dat$n.stim,dat$n.unstim)
+	llnull<-cmpfun(MIMOSA:::makeLogLikeNULLComponent(dat$n.stim,dat$n.unstim))
+	llresp<-cmpfun(MIMOSA:::makeLogLikeRespComponent(dat$n.stim,dat$n.unstim))
 	
 	S.stim<-1000
 	S.unstim<-1000
 	w.u<-rowMeans(apply(dat$n.unstim,1,prop.table))
 	w.s<-rowMeans(apply(dat$n.stim,1,prop.table))
-	#w.u=rdirichlet(1,rep(1,length(alpha.u)))
-	#w.s=rdirichlet(1,rep(1,length(alpha.s)))
+	alpha.s<-w.s*S.stim
+	alpha.u<-w.u*S.unstim
+	S.s<-S.u<-diag(1,length(alpha.s))
+	Cs<-Cu<-2.34/sqrt(length(alpha.s))
 	q<-runif(1)
-	z<-MIMOSA:::simZ(q,w.s,w.u,S.stim,S.unstim)
-	f<-file("mcmc.dat",open="wt",blocking=FALSE)
-	writeLines(paste(c(paste("ws",1:length(w.s),sep="",collapse="\t"),paste("wu",1:length(w.u),sep="",collapse="\t"),"S.stim","S.unstim","q","\n"),collapse="\t"),f)
-	writeLines(text=as.character(c(w.s,w.u,S.unstim,S.stim,q,"\n")),sep="\t",f)
-	#coda needs the order reversed. Bizarre
+	z<-MIMOSA:::simZ(q,w.s,w.u,S.stim,S.unstim,llnull,llresp)
+	f<-matrix(0,nrow=iter,ncol=length(w.s)+length(w.u)+1)
+	cn<-c(paste("alpha.s",1:length(w.s),sep=""),paste("alpha.u",1:length(w.u),sep=""),"q")
+	colnames(f)<-cn
+	#writeLines(text=as.character(c(alpha.s,alpha.u,q,"\n")),sep="\t",f)
+	tune=100
+	rate.s<-0
+	rate.u<-0
+	fixed=FALSE
 	for(i in 1:iter){
-		w.s<-MIMOSA:::simWs(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,w.stepS,llnull,llresp)
-		w.u<-MIMOSA:::simWu(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,w.stepU,llnull,llresp)
-		S.unstim<-MIMOSA:::simS.unstim(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,sigmaU,llnull,llresp)
-		S.stim<-MIMOSA:::simS.stim(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,sigmaS,llnull,llresp)
-		z<-MIMOSA:::simZ(q=q,w.s=w.s,w.u=w.u,S.stim=S.stim,S.unstim=S.unstim,llnull,llresp)
+		#w.s<-MIMOSA:::simWs(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,w.stepS,llnull,llresp)
+		#w.u<-MIMOSA:::simWu(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,w.stepU,llnull,llresp)
+		#S.unstim<-MIMOSA:::simS.unstim(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,sigmaU,llnull,llresp)
+		#S.stim<-MIMOSA:::simS.stim(w.s=w.s,w.u=w.u,z=z,S.stim=S.stim,S.unstim=S.unstim,sigmaS,llnull,llresp)
+
+		#every 100 iterations do some tuning
+		if(i%%tune==0){
+			#browser()
+			rate.s<-rate.s/tune
+			rate.u<-rate.u/tune
+			if(!(rate.s<=0.26&rate.s>=0.22&rate.u>=0.22&rate.u<=0.26)&!fixed){
+				cat("rate.s=",rate.s," rate.u=",rate.u,"\n");
+				Cs=Cs*abs(qnorm(0.24/2)/qnorm(rate.s/2))
+				Cu=Cu*abs(qnorm(0.24/2)/qnorm(rate.u/2))
+				S.s<-S.s*0.75+0.25*(cov(f[(i-tune+1):i,1:length(alpha.s)]))
+				S.u<-S.u*0.75+0.25*(cov(f[(i-tune+1):i,(length(alpha.s)+1):(length(alpha.s)+length(alpha.u))]))
+				rate.s<-rate.u<-0
+			} else if(!fixed) {
+				cat("rate.s=",rate.s," rate.u=",rate.u,"\n");
+				cat("Fixing the step size and sampling ", iter, " samples with a burn-in of ", burn,"\n")
+				fixed<-TRUE
+				i<-1
+			}
+		}
+		ret<-MIMOSA:::simAlpha.s(alpha.s,alpha.u,z,S.s,C=Cs,llnull,llresp,rate.s)
+		alpha.s<-ret[[1]]
+		rate.s<-ret[[2]]
+		ret<-MIMOSA:::simAlpha.u(alpha.s,alpha.u,z,S.u,C=Cu,llnull,llresp,rate.u)
+		alpha.u<-ret[[1]]
+		rate.u<-ret[[2]]
+		z<-MIMOSA:::simZ(q=q,w.s=alpha.s*sum(alpha.s),w.u=alpha.u*sum(alpha.u),S.stim=sum(alpha.s),S.unstim=sum(alpha.u),llnull,llresp)
 		if(i==burn){
 				sz=z
 		}else if(i>burn){
 			sz=(sz+z/(i-1))*((i-1)/i)	
 		}
 		q<-MIMOSA:::simQ(z=z)
-		writeLines(text=as.character(c(w.s,w.u,S.unstim,S.stim,q,"\n")),sep="\t",f)
+		f[i,]<-c(alpha.s,alpha.u,q)
 	}
-	close(f)
-	r=read.table("mcmc.dat",header=T)
-	r=r[nrow(r):1,]
-	return(list(z=sz,mcmc=mcmc(r,start=burn,end=iter)))
+	write.table(f,file="mcmc.dat")
+	return(list(z=sz,mcmc=mcmc(f[burn:iter,])))
 }
