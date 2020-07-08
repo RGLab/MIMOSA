50d7dbdf4a2d3d95600bb0ab2e43dec78ce8132e
Greg Finak <gfinak@fredhutch.org>
2020-07-08 15:01:46 -0700

    Fix #11

--- R/posteriorRR.R
+++ R/posteriorRR.R
@@ -10,16 +10,24 @@
 #'
 #'Future versions will allow passing of quantiles for summarization, and maybe the full distribution, depending on needs.
 #'@param x A MIMOSAResultList or MIMOSAResult. All models should be of type MCMCResult, or fitted using method="mcmc" in MIMOSA.
-#'@param variable an unquoted variable name in the pData() table of all the models that specifies the grouping variable for which to compute response rates.
+#'@param ... unquoted grouping variable name in the pData() table of all the models that specifies the one or more grouping variable by which to compute response rates.
 #'@return a tibble with the grouping variable, and the 2.5th, 25th,  median, 75th, and 97.5th percentiles of the posterior response rate.
 #'@import tidyr
 #'@importFrom dplyr bind_rows bind_cols group_by summarize ungroup `%>%`
 # @importFrom plyr ldply
 #'@export
-getPosteriorResponseRate <- function(x, variable) {
-  quo_variable <- enquo(variable)
+getPosteriorResponseRate <- function(x, ...) {
+  quo_variable <- enquos(...)
+  if(length(quo_variable)>0){
+	if(Reduce(any,lapply(quo_variable,function(x)grepl("\"",rlang::quo_text(x))))) 	{
+		stop("Variables should not be quoted")
+	}
+  	if(any(!names(lapply(quo_variable, function(x) rlang::quo_text(x)))%in%""))	{
+		stop("Variables should not be named")
+	}
+  }
   if ("MIMOSAResultList" %in% class(x)) {
-      retme<-lapply(x,function(x)getPosteriorResponseRate(x,!!quo_variable))
+      retme<-lapply(x,function(x)getPosteriorResponseRate(x,...))
       retme<-plyr::ldply(retme,.id="model")
 #      retme2<-dplyr::bind_rows(retme)
 #      retme<-dplyr::bind_cols(retme2,model=names(retme))
@@ -29,8 +37,8 @@ getPosteriorResponseRate <- function(x, variable) {
     cn <- data.frame(rn = colnames(mat))
     pd <- pData(x)
     groups <-
-      pd %>% dplyr::select(!!quo_variable) %>% dplyr::bind_cols(rn = rownames(pd))
-    posterior_rr<-dplyr::right_join(groups, cn) %>% dplyr::group_by(!!quo_variable) %>%
+      pd %>% dplyr::select(...) %>% dplyr::bind_cols(rn = rownames(pd))
+    posterior_rr<-dplyr::right_join(groups, cn) %>% dplyr::group_by(...) %>%
       dplyr::do(., {
         data.frame(rr = rowMeans(mat[, .$rn])) %>% dplyr::summarize(
           Q2.5 = quantile(rr, 0.025),
